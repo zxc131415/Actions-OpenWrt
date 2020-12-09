b430554c20d4b5dd804501d8017050ac852bd5df
zxc131415 <40189636+zxc131415@users.noreply.github.com>
2020-12-09 15:23:39 +0800

    Create k2pdiy.sh

---
+++ k2pdiy.sh
@@ -0,0 +1,5 @@
+rm -rf package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/myf/argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/myf/argon-config
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/myf/jd
+./scripts/feeds update -a && ./scripts/feeds install -a
