a6ff4bd2d74865b4ff30b8da54e302a325b94ffb
zxc131415 <40189636+zxc131415@users.noreply.github.com>
2020-12-09 16:14:47 +0800

    Update x86diy.sh

--- x86diy.sh
+++ x86diy.sh
@@ -1,6 +1,6 @@
 rm -rf package/lean/luci-theme-argon
 git clone https://github.com/frainzy1477/luci-app-clash.git package/myf/clash
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/myf/argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/myf/argon-config
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/myf/jd
 git clone https://github.com/tty228/luci-app-serverchan.git package/myf/serverchan
