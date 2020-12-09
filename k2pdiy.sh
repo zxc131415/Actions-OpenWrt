4aa52e671c02ad63f94c116371fee2989fcdd493
zxc131415 <40189636+zxc131415@users.noreply.github.com>
2020-12-09 19:11:30 +0800

    Update k2pdiy.sh

--- k2pdiy.sh
+++ k2pdiy.sh
@@ -1,5 +1,5 @@
 rm -rf package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/myf/argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/myf/argon-config
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/myf/jd
 ./scripts/feeds update -a && ./scripts/feeds install -a
