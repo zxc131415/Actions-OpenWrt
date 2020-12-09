ec2719acebbf6f135746984f51067b03f4b21fe7
zxc131415 <40189636+zxc131415@users.noreply.github.com>
2020-12-09 15:03:28 +0800

    Create diy.sh

---
+++ x86-64/diy.sh
@@ -0,0 +1,8 @@
+rm -rf package/lean/luci-theme-argon
+git clone https://github.com/frainzy1477/luci-app-clash.git package/myf/clash
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/myf/argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/myf/argon-config
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/myf/jd
+git clone https://github.com/tty228/luci-app-serverchan.git package/myf/serverchan
+git clone https://github.com/vernesong/OpenClash.git package/myf/openclash
+./scripts/feeds update -a && ./scripts/feeds install -a
